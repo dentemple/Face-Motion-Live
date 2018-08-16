ea71192964e2917991bab0edc04e02cc7f06cace
Kwame Amosah <kwameamosah@Kwames-MacBook-Pro.local>
2018-08-15 20:37:11 -0400

    update 4

--- background.js
+++ background.js
@@ -1,2 +1,8 @@
-var background = chrome.extension.getBackgroundPage()
-background.console.log("Test")
+// var background = chrome.extension.getBackgroundPage()
+// background.console.log("Test");
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
