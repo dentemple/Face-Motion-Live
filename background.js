e89425a1861efd5e6a0ad00d4c4b610c3583b96b
Den Temple <developer@dentemple.com>
2018-08-16 03:40:42 -0400

    Resolve merge conflicts

--- background.js
+++ background.js
@@ -2,60 +2,41 @@
 // background.console.log("Test");
 
 chrome.runtime.onInstalled.addListener(function() {
-<<<<<<< HEAD
   chrome.storage.sync.set({ color: "#3aa757" }, function() {
     console.log("The color is green.")
   })
 })
 
-chrome.history.search({ text: "", maxResults: 1000 }, page => {
-  chrome.extension.onConnect.addListener(function(port) {
-    port.postMessage(page.slice(0, 5))
-  })
-})
-=======
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
-
 // let history = {};
- 
-chrome.history.search({ text: ""}, page => {
-    
-    function getSiteName (url) {
-        let start = url.indexOf("//");
-        let end = url.indexOf("/", start+2)+1;
-        return url.slice(start+2, end-1);
+
+chrome.history.search({ text: "" }, page => {
+  function getSiteName(url) {
+    let start = url.indexOf("//")
+    let end = url.indexOf("/", start + 2) + 1
+    return url.slice(start + 2, end - 1)
+  }
+
+  function getUrls(urlHist) {
+    let browserHistory = {}
+    console.log(urlHist)
+    for (let i = 0; i < urlHist.length; i++) {
+      let date = new Date(urlHist[i].lastVisitTime * 1000)
+      let minutes = date.getHours() * 60 + date.getMinutes()
+      let webName = getSiteName(urlHist[i].url)
+      // console.log(webName);
+
+      if (browserHistory.hasOwnProperty(webName)) {
+        let currTime = browserHistory[webName].currTimeinMin
+        console.log({ minutes, currTime })
+        browserHistory[webName].timeSpent += minutes - browserHistory[webName].currTimeinMin
+        browserHistory[webName].currTimeinMin = minutes
+      } else {
+        browserHistory[webName] = { currTimeinMin: minutes, timeSpent: 0 }
+      }
     }
+    console.log(browserHistory)
+    return browserHistory
+  }
 
-    function getUrls (urlHist) {
-        let browserHistory = {};
-        console.log(urlHist);
-        for(let i = 0; i < urlHist.length; i++) {
-            let date = new Date(urlHist[i].lastVisitTime * 1000);
-            let minutes = (date.getHours() * 60) + (date.getMinutes());
-            let webName = getSiteName(urlHist[i].url);
-            // console.log(webName);
-            
-            
-            if(browserHistory.hasOwnProperty(webName)) {
-                let currTime = browserHistory[webName].currTimeinMin;
-                console.log({minutes, currTime})
-                    browserHistory[webName].timeSpent += (minutes - browserHistory[webName].currTimeinMin);
-                    browserHistory[webName].currTimeinMin = minutes;
-            } else {
-                browserHistory[webName] = {currTimeinMin: minutes, timeSpent: 0};
-            }
-            
-        }
-        console.log(browserHistory);
-        return browserHistory;
-    }
-    
-    return getUrls(page);
-});
-
-
-
->>>>>>> 5c31c7258cd0434b339c7ae1cc8768b55dbf52db
+  return getUrls(page)
+})
