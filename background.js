606fd45199b43e20f14fe43de2294ff16a14bc96
Den Temple <developer@dentemple.com>
2018-08-15 20:37:15 -0400

    Changes

--- background.js
+++ background.js
@@ -1,2 +1,12 @@
 var background = chrome.extension.getBackgroundPage()
 background.console.log("Test")
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create("popup.html", {
+    id: "camCaptureID",
+    innerBounds: {
+      width: 700,
+      height: 600
+    }
+  })
+})
